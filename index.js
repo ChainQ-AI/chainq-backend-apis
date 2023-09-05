fade24eae40fd9563975f73050193d15f93b0ed0
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2023-09-05 15:43:50 +0530

    default routes exempted

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(
   expressJwt({
     secret: JWT_SECRET_KEY,
     algorithms: ["HS256"],
-  }).unless({ path: ["/login"] })
+  }).unless({ path: ["/login", "/"] })
 );
 
 // Error handling middleware for invalid tokens
